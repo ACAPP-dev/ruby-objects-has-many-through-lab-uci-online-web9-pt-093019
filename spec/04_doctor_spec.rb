490601dfc5785613eb151e0d432013d31d177de6
Andrew Capp <acapp909@gmail.com>
2019-11-06 06:26:14 +0000

    Automatically backed up by Learn

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -23,7 +23,7 @@ describe 'Doctor' do
       hevydevy = Patient.new('Devin Townsend')
       appointment = Appointment.new('Friday, January 32nd', hevydevy, doctor_who)
       appointment_2 = Appointment.new('Saturday, January 33rd', hevydevy, doctor_smith)
-      
+
       expect(doctor_who.appointments).to include(appointment)
       expect(doctor_smith.appointments).to include(appointment_2)
 
@@ -44,7 +44,7 @@ describe 'Doctor' do
     it 'has many patients, through appointments' do
       doctor_who = Doctor.new('The Doctor')
       hevydevy = Patient.new('Devin Townsend')
-      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
+      doctor_who.new_appointment('Friday, January 32nd', hevydevy)
 
       expect(doctor_who.patients).to include(hevydevy)
     end
