589675c2e9603ca3ced4366beb64dd1e1b36ac19
Andrew Capp <acapp909@gmail.com>
2019-11-06 05:34:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
   end
 
   def songs
-    artist_array = Song.all.select{|song_instance| song_instance.artist == self}
+    Song.all.select{|song_instance| song_instance.artist == self}
   end
 
   def genres
