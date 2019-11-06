b7261c8026e4d355f25bf7ea6203879912f7ea5f
Andrew Capp <acapp909@gmail.com>
2019-11-06 06:21:31 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Doctor
 
   attr_accessor :name
@@ -13,6 +14,7 @@ class Doctor
   end
 
   def appointments
+    binding.pry
     Appointment.all.select {|appt_instance| appt_instance.doctor == self}
   end
 
